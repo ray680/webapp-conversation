69b64965df2ad08bcba93d5f4283a4e1437eb93c
ray680 <136893708+ray680@users.noreply.github.com>
2023-06-17 18:40:26 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = '0115dfc4-d496-47d1-8df7-42eab326a7e9'
+export const API_KEY = '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'
+export const API_URL = 'https://udify.app/chat/DGk5Er9jH3i1LdDi'
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
